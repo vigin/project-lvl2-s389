5c8262e3d067ccecaf68dea99ea1400e8d1d6d9a
Sergey Ermakov <sergey.a.ermakov@gmail.com>
2018-09-19 16:53:47 +0300

    step 1

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,20 +1,13 @@
-var program = require('commander');
+const program = require('commander');
 
+export const runHelp = () => {
+  program
+    .version('1.0.0', '-V, --version', 'output the version number')
+    .arguments('<firstConfig> <secondConfig>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format');
 
+  program.parse(process.argv);
+};
 
-  Usage: gendiff [options] <firstConfig> <secondConfig>
-
-  Compares two configuration files and shows a difference.
-
-program
-  .version('1.0.0')
-  .option('-h, --help', 'output usage information')
-  .option('-V, --version', 'Add pineapple')
-  .option('-f, --format [type]', 'Output format')
-  .parse(process.argv);
-
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+export default runHelp;
